e9227a31337be1b844e7dd2d9e8934dc578f7737
aubriebarnhart <aubrieb@live.unc.edu>
2021-11-22 08:56:44 -0500

    first couple from lecture

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
-import { sumToString } from "./src/mild/mild_1";
+import { getIncreasingArray, sumToString } from "./src/mild/mild_1.js";
 
 console.log(sumToString(3,4));
+
+console.log(getIncreasingArray(3,7));
