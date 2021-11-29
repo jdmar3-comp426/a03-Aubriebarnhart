04e87fa95cabafa27873107e97548a2d01c13db7
aubriebarnhart <aubrieb@live.unc.edu>
2021-11-29 14:18:03 -0500

    added allEvens and anEven functions

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -174,7 +174,14 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    function test (element) {
+        if (element % 2 == 1) {
+            return true;
+        } else if (element % 2 == 0) {
+            return false;
+        }
+    }
+    return everyEven(arr, test);
 };
 
 
@@ -184,7 +191,14 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    function test (element) {
+        if (element % 2 == 1) {
+            return true;
+        } else if (element % 2 == 0) {
+            return false;
+        }
+    }
+    return someEven(arr, test);
 };
 
 
